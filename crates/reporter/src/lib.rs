7fa4d9aaf12398f8850e8bc365f0ce5af9b049d9
eason <eason@brevis.network>
2025-10-08 19:37:05 +0800

    implement websocket reporter

--- crates/reporter/src/lib.rs
+++ crates/reporter/src/lib.rs
@@ -1,19 +1,43 @@
 use derive_more::Constructor;
-use messages::BlockMsgReceiver;
+use messages::{BlockMsg, BlockMsgReceiver, WatchMsg};
 use std::sync::Arc;
-use tokio::{spawn, task::JoinHandle};
-use tracing::info;
+use tokio::task::{JoinHandle, spawn_blocking};
+use tracing::{error, info};
 
 #[derive(Constructor, Debug)]
 pub struct BlockReporter {
     // communication receiver for coordinating with the main scheduler
-    pub comm_sender: Arc<BlockMsgReceiver>,
+    pub comm_receiver: Arc<BlockMsgReceiver>,
 }
 
 impl BlockReporter {
     pub fn run(self: Arc<Self>) -> JoinHandle<()> {
         info!("reporter: start");
 
-        spawn(async move { tokio::time::sleep(tokio::time::Duration::from_secs(60_000)).await })
+        spawn_blocking(move || {
+            // saving the websocket watchers and will be removed as close if notification failed
+            let mut watchers = vec![];
+            while let Ok(msg) = self.comm_receiver.recv() {
+                match &msg {
+                    BlockMsg::Watch(WatchMsg { sender }) => {
+                        watchers.push(sender.clone());
+                        info!(
+                            "reporter: added a new websocket watcher, the current watcher number is {}",
+                            watchers.len(),
+                        );
+                    }
+                    BlockMsg::Report(report) => {
+                        let block_number = report.block_number;
+                        watchers.retain(|watcher| watcher.send(msg.clone()).is_ok());
+                        info!(
+                            "reporter: notified the proved block {block_number} to watcher number {}",
+                            watchers.len(),
+                        );
+                    }
+                    _ => error!("proving-client: received a wrong message {msg:?}"),
+                }
+            }
+            info!("reporter: stopped");
+        })
     }
 }
