fc1aeccb2aa5a4ad719f2483fca7068aaf68b5fa
Eason <145818884+eason1981@users.noreply.github.com>
2025-10-09 12:57:55 +0800

    add subblock public values to aggregator proto and generated from fetcher thread (#12)
    Co-authored-by: eason <eason@brevis.network>

--- crates/common/src/inputs.rs
+++ crates/common/src/inputs.rs
@@ -5,6 +5,9 @@ pub struct ProvingInputs {
     // block number to prove
     pub block_number: u64,
 
+    // subblock public values
+    pub subblock_public_values: Vec<Vec<u8>>,
+
     // bincode serialized aggregation stdin builder
     pub agg_input: Vec<u8>,
 
